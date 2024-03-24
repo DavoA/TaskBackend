3f125fe9c01f41cccc646dbdbaaa794e55ceb46e
David Aristakesyan <45460144+DavoA@users.noreply.github.com>
2024-03-24 14:04:46 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,13 @@
-require('dotenv').config();
-
 const express = require("express");
 const cors = require('cors');
 const mongoose = require("mongoose");
 const port = 3001;
 const routes = require("./routes");
 
-const mongodbHost = process.env.MONGO_HOST;
-const mongodbPort = process.env.MONGO_PORT;
-const mongodbDb = process.env.MONGO_DB;
-
-const mongodbUrl = `mongodb://${mongodbHost}:${mongodbPort}/${mongodbDb};`
-
-
 main().catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect(mongodbUrl, {
+  await mongoose.connect(`${process.env.MONGO_HOST}`, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
   });
